1fad25b7eb5dbd0a29d95f3d8838cf01a1e8f935
DESKTOP-3M2FJBS\USUARIO <isabellanaja0@gmail.com>
2020-11-10 17:15:22 +0100

    commit inicial

---
+++ src/main/java/com/example/RetoIsabelMS/RetoIsabelMsApplication.java
@@ -0,0 +1,13 @@
+package com.example.RetoIsabelMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetoIsabelMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetoIsabelMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/RetoIsabelMS/RetoIsabelMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RetoIsabelMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetoIsabelMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
